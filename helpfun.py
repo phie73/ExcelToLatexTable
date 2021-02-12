48c73157630f46fef08a6d8af58cf7e9cc6869eb
43-50ph14 <so_s@web.de>
2021-02-12 19:56:00 +0100

    including of csv files to latex, beginning of exampel.pdf/.tex

---
+++ helpfun.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+#get file path
+def filepath(destination, filename, mode):
+    if mode == 0:
+        if destination != None:
+            return destination
+        else:
+            x = filename.rfind('/')
+            return filename[0:x] + '/table.tex'
+    if mode == 1:
+        x = filename.rfind('/')
+        return filename[0:x] + '/csvfile.csv'
+    else:
+        print('fun didn\'t work this way')
+        return -1
+
+
+#csvsumple packag
+def csvsimple(tex_file, csv_file):
+    f = open(tex_file, 'a')
+    s = '\\csvautotabular{' + csv_file + '}'
+    f.write(s)
+    f.close()
+
+
+#convert to csv
+def to_csv(excel_file, csv_file, sheet):
+    data_xls = pd.read_excel(excel_file, sheet, dtype=str, index_col=None)
+    data_xls.to_csv(csv_file, encoding='utf-8', index=False, header=False)
--- tool.py
+++ tool.py
@@ -1,23 +1,23 @@
-import pandas as pd 
 import argparse
+import os 
 from helpfun import*
 
 # parsing command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file',
-    help='filepath of excel file'
+    help='filepath of excel, csv file file'
 )
 #todo create tex file as default location of tex table
 parser.add_argument('-d','--destination',
     default=None,
-    help='[optional]: destination of .tex file with table \nDefault: new .txt file in same dir as excel file'
+    help='[optional]: destination of .tex file with table \nDefault: new table.tex file in same dir as excel file'
 )
 parser.add_argument('-s', '--sheet',
-    default='Sheet 1',
-    help='[oprional]: sheet in excel file \nDefault: Sheet 1'
+    default='Sheet1',
+    help='[oprional]: sheet in excel file \nDefault: Sheet1'
 )
 parser.add_argument('-t', '--tabletypes',
-    default=None
+    default=None,
     help='show all aviable types of latex tables'
 )
 parser.add_argument('-ct', '--choose_tabletypes',
@@ -27,13 +27,26 @@ parser.add_argument('-ct', '--choose_tabletypes',
 args = parser.parse_args()
 
 #save command line arguments in vars
-excel_file =  args.file
+given_file =  args.file
 destination = args.destination
 sheet = args.sheet
 show_table_types = args.tabletypes
 choosen_table_type = args.choose_tabletypes
 
+#detrmine filetype, extracting path 
+filename, file_extension = os.path.splitext(given_file)
+table_file  = filepath(destination, filename, 0)
+csv_file_path = filepath(destination, filename, 1)
+if file_extension == '.csv' or file_extension == '.xlsx':
+    if file_extension == '.xlsx':
+        to_csv(given_file, csv_file_path, sheet)
+        given_file = csv_file_path
+    if choosen_table_type == '1':
+        csvsimple(table_file, given_file)
 
-# excel to csv
-# data_xls = pd.read_excel(excel_file, dtype=str, index_col=None)
-# data_xls.to_csv('csvfile.csv', encoding='utf-8', index=False, header=False)
+    
+else:
+    raise Exception('file should be either an .csv or .xlsx file. Given was {}'.format(file_extension))
+
+
+
